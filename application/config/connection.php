91e8c3fb35eaf54e44bd191c90db4f53e2ec1cf0
denisdst <35616931+denisdst@users.noreply.github.com>
2018-01-27 00:12:01 +0700

    Update connection.php

--- application/config/connection.php
+++ application/config/connection.php
@@ -1,11 +1,6 @@
-<?php
-$SERVER_SERVER = "localhost";
-$SERVER_USER = "root";
-$SERVER_PASSWORD = "";
-$DATABASE_NAME = "project_marhas";
-$con = mysqli_connect($SERVER_SERVER,$SERVER_USER,$SERVER_PASSWORD, $DATABASE_NAME);
-	if (mysqli_connect_errno())
-	{
-		echo "Failed to Connect to Database: " . mysqli_connect_error();
-	}
+<?php 
+$mysqli = new mysqli('localhost','root','','project_marhas');
+if($mysqli->connect_errno){
+echo "I'm Sorry.. Connection Failed!".$mysqli->connect_error;
+}
 ?>
